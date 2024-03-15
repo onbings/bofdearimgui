c74bfb2aab6f2a00ebdc46c026ce8de872ab56aa
HARMEL, Bernard <b.harmel@evs.com>
2024-03-15 12:50:39 +0100

    update cpack

--- cmake/cpack_rpm.cmake
+++ cmake/cpack_rpm.cmake
@@ -6,7 +6,7 @@ if (UNIX AND CMAKE_SYSTEM_NAME MATCHES Linux)
       list(APPEND CPACK_GENERATOR        "RPM")
       set(CPACK_RPM_PACKAGE_NAME         "${PROJECT_NAME}${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
       set(CPACK_RPM_PACKAGE_RELEASE      "${VERSION_LABEL}")
-      set(CPACK_RPM_PACKAGE_LICENSE      "Onbings")
+      set(CPACK_RPM_PACKAGE_LICENSE      "OnBings")
       set(CPACK_RPM_PACAKGE_RELOCATABLE  ON)
       set(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
 
@@ -24,15 +24,11 @@ if (UNIX AND CMAKE_SYSTEM_NAME MATCHES Linux)
          set(OS_NAME centos)
       endif()
 
-      set(rpm_releasePkgName   "${CPACK_RPM_PACKAGE_NAME}-${PROJECT_VERSION}-${VERSION_LABEL}-${RHEL}.${OS_NAME}.evs.rpm")
-      set(rpm_debugPkgName     "${CPACK_RPM_PACKAGE_NAME}-debug-${PROJECT_VERSION}-${VERSION_LABEL}-${RHEL}.${OS_NAME}.evs.rpm")
-      set(rpm_develPkgName     "${CPACK_RPM_PACKAGE_NAME}-devel-${PROJECT_VERSION}-${VERSION_LABEL}.rpm")
-   
       set(CPACK_RPM_COMPONENT_INSTALL        ON)
 
-      set(CPACK_RPM_RUNTIME_FILE_NAME        ${rpm_releasePkgName})
-      set(CPACK_RPM_DEVEL_FILE_NAME          ${rpm_develPkgName})
-      set(CPACK_RPM_DEBUG_FILE_NAME          ${rpm_debugPkgName})
+      set(CPACK_RPM_RUNTIME_FILE_NAME        ${CPACK_RPM_PACKAGE_NAME}-${PROJECT_VERSION}-${VERSION_LABEL}-${RHEL}.${OS_NAME}.onbings.rpm)
+      set(CPACK_RPM_DEVEL_FILE_NAME          ${CPACK_RPM_PACKAGE_NAME}-devel-${PROJECT_VERSION}-${VERSION_LABEL}.rpm)
+      set(CPACK_RPM_DEBUG_FILE_NAME          ${CPACK_RPM_PACKAGE_NAME}-debug-${PROJECT_VERSION}-${VERSION_LABEL}-${RHEL}.${OS_NAME}.onbings.rpm)
          
       set(CPACK_RPM_RUNTIME_PACKAGE_NAME     ${CPACK_RPM_PACKAGE_NAME})
       set(CPACK_RPM_DEVEL_PACKAGE_NAME       ${CPACK_RPM_PACKAGE_NAME}-devel)
