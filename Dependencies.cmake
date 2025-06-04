ba236754a19cd9002108a899a917624bc3a70eb9
HARMEL, Bernard <b.harmel@evs.com>
2025-06-04 13:41:03 +0200

    align baseline

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -19,7 +19,8 @@ endmacro()
 # == REQUIRED DEPENDENCIES ==
 #find_package(GTest CONFIG REQUIRED)
 find_package(bofstd CONFIG REQUIRED)
-find_package(sdl2-core CONFIG REQUIRED)
+#find_package(sdl2-core CONFIG REQUIRED)  
+find_package(SDL2 CONFIG REQUIRED)  #and not sdl2-core as it is only an overwrite of official sdl2 with other option by default
 find_package(hello-imgui CONFIG REQUIRED)
 find_package(implot CONFIG REQUIRED)
 #if find_package, add   {"name": "hello-imgui", "features": [ "opengl3-binding", "sdl2-binding"] } to vcpkg.json
